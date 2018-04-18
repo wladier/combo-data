190b6c7d9a6f3f29aa623c1360cf0b1c0219fa18
Aitor Garcia <aitor.garcia.miranda@gmail.com>
2018-04-18 07:51:30 +0200

    'subida'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ComboDataModule } from "./combo-data/combo-data.module";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComboDataModule
   ],
   providers: [],
   bootstrap: [AppComponent]
